08debc9658622d2656ae4744be932621d0fd279c
Alvaro Diaz-Ruelas <alvaro.diaz.ruelas@gmail.com>
2023-04-30 16:41:36 +0200

    setup.py and draft of first notebook created

---
+++ _rb/setup.py
@@ -0,0 +1,12 @@
+# setup
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+import scipy
+import statsmodels
+import warnings
